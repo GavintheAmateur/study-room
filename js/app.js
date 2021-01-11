1fb33c4624267146a98fd4ae9d7ad0a6f4c9ceef
Gavin Li <gali.aligntech.com>
2021-01-11 13:43:02 +0800

    update

--- js/app.js
+++ js/app.js
@@ -5,56 +5,79 @@
 // }
 
 const playCD = () => {
-  const cd = document.querySelector(".desk-cd-player");
-  if (!cd.paused) {
-     cd.pause();
-  } else {
-    cd.play();
-  }
+    const cd = document.querySelector(".desk-cd-player");
+    if (!cd.paused) {
+        cd.pause();
+    } else {
+        cd.play();
+    }
 }
 
 
-
-const showAlbum = ()=> {
-  const album = document.querySelector(".album-modal");
-  album.style.display='block';
+const showAlbum = () => {
+    const album = document.querySelector(".album-modal");
+    album.style.display = 'block';
 }
 
-const closeAlbum = ()=> {
-  const album = document.querySelector(".album-modal");
-  album.style.display='none';
-}
-
-
-const changeBackground = ()=> {
-  const bg = document.querySelector(".light-modal");
-  if (bg.style.display==='none') {
-    bg.style.display='block';
-  } else {
-    bg.style.display='none';
-  }
-}
-
-const initListeners = ()=> {
-  // document.addEventListener('dblclick',removeItems);
-
-const ele= document.querySelector('.desk-cd');
-ele.addEventListener('click', playCD);
-
-const album = document.querySelector(".desk-album");
-album.addEventListener('click',showAlbum);
-
-const album_modal = document.querySelector(".album-modal-content");
-album_modal.addEventListener('click',closeAlbum);
-
-const lamp = document.querySelector(".desk-lamp");
-lamp.addEventListener('click',changeBackground);
-
-const light_modal = document.querySelector(".light-modal");
-light_modal.addEventListener('click',changeBackground);
-
-
+const closeAlbum = () => {
+    const album = document.querySelector(".album-modal");
+    album.style.display = 'none';
 }
 
 
-initListeners();``
+const changeBackground = () => {
+    const bg = document.querySelector(".light-modal");
+    if (bg.style.display === 'none') {
+        bg.style.display = 'block';
+    } else {
+        bg.style.display = 'none';
+    }
+}
+
+const openEditor = () => {
+    const bg = document.querySelector(".editor-modal");
+    bg.style.display = 'block';
+
+
+    tinymce.init({
+        selector: '.editor-body',
+        height: '100%'
+    });
+
+}
+
+const closeEditor = () => {
+    const editor = document.querySelector(".editor-modal");
+    editor.style.display = 'none';
+}
+
+const initListeners = () => {
+    // document.addEventListener('dblclick',removeItems);
+
+    const ele = document.querySelector('.desk-cd');
+    ele.addEventListener('click', playCD);
+
+    const album = document.querySelector(".desk-album");
+    album.addEventListener('click', showAlbum);
+
+    const album_modal = document.querySelector(".album-modal-content");
+    album_modal.addEventListener('click', closeAlbum);
+
+    const lamp = document.querySelector(".desk-lamp");
+    lamp.addEventListener('click', changeBackground);
+
+    const light_modal = document.querySelector(".light-modal");
+    light_modal.addEventListener('click', changeBackground);
+
+
+    const notebook = document.querySelector(".desk-notebook");
+    notebook.addEventListener('click', openEditor);
+
+    const editor_modal = document.querySelector(".editor-modal");
+    editor_modal.addEventListener('click', closeEditor);
+
+}
+
+
+initListeners();
+``
